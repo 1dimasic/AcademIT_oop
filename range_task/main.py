7f80d46b00669c336860eaf5045a3965bdb4f2aa
dimasic <litrox@mail.ru>
2023-07-24 16:35:53 +0700

    Внесены изменения: 1. Пересечение, объединение и разность сделаны без пользования is_inside и изменена выдача результата при пересечении по одной точке. 2. В get_intersection добавлен return.

--- range_task/main.py
+++ range_task/main.py
@@ -1,5 +1,23 @@
 from range import Range
 
+# тестовая программа для проверки класса Range
+start_point = float(input('Введите начало интервала: '))
+end_point = float(input('Введите конец интервала: '))
+point_a = float(input('Введите координату точки А: '))
+
+current_range = Range(start_point, end_point)
+
+if current_range.is_inside(point_a):
+    print(f'Точка A находится внутри интервала {current_range}')
+    range_1 = Range(start_point, point_a)
+    range_2 = Range(point_a, end_point)
+    print(
+        f'Расстояние от начала интервала до точки A = {range_1.length:.2f}, '
+        f'расстояние от точки A до конца интервала = {range_2.length:.2f}')
+else:
+    print(f'Точка А не находится внутри интервала {current_range}')
+
+# тестовая программа для проверки методов класса Range*
 start_1 = float(input('Введите начало первого интервала: '))
 end_1 = float(input('Введите конец первого интервала: '))
 start_2 = float(input('Введите начало второго интервала: '))
--- range_task/range.py
+++ range_task/range.py
@@ -5,6 +5,7 @@ class Range:
 
         self.__start = start
         self.__end = end
+        self.__length = end - start
 
     @property
     def start(self):
@@ -22,60 +23,39 @@ class Range:
     def end(self, end):
         self.__end = end
 
-    def get_length(self):
-        return self.__end - self.__start
+    @property
+    def length(self):
+        return self.__length
 
     def is_inside(self, point):
         return self.__start <= point <= self.__end
 
-    def get_intersection(self, verifiable_range):
-        if self.is_inside(verifiable_range.__start):
-            return Range(verifiable_range.__start, min(self.__end, verifiable_range.__end))
+    def get_intersection(self, range_2):
+        if max(self.__start, range_2.__start) < min(self.__end, range_2.__end):
+            return Range(max(self.__start, range_2.__start), min(self.__end, range_2.__end))
 
-        if verifiable_range.is_inside(self.__start):
-            return Range(self.__start, min(self.__end, verifiable_range.__end))
+        return None
 
-    def get_union(self, verifiable_range):
-        if self.is_inside(verifiable_range.__start):
-            return Range(self.__start, max(self.__end, verifiable_range.__end))
+    def get_union(self, range_2):
+        if max(self.__start, range_2.__start) <= min(self.__end, range_2.__end):
+            return Range(min(self.__start, range_2.__start), max(self.__end, range_2.__end))
 
-        if verifiable_range.is_inside(self.__start):
-            return Range(verifiable_range.__start, max(self.__end, verifiable_range.__end))
+        return [Range(self.__start, self.__end), Range(range_2.__start, range_2.__end)]
 
-        return [self, verifiable_range]
+    def get_difference(self, range_2):
+        if self.__start > range_2.__start and self.__end < range_2.__end:
+            return []
 
-    def get_difference(self, verifiable_range):
-        if verifiable_range.is_inside(self.__start) and verifiable_range.is_inside(self.__end):
-            return Range(0, 0)
+        if self.__start < range_2.__start and self.__end > range_2.__end:
+            return Range(self.__start, range_2.__start)
 
-        if self.is_inside(verifiable_range.__start) and self.is_inside(verifiable_range.__end):
-            return [Range(self.__start, verifiable_range.__start), Range(verifiable_range.__end, self.__end)]
+        if self.__start < range_2.__start < self.__end:
+            return Range(self.__start, range_2.__start)
 
-        if self.is_inside(verifiable_range.__start):
-            return Range(self.__start, verifiable_range.__start)
+        if self.__start < range_2.__end < self.__end:
+            return Range(range_2.__end, self.__end)
 
-        if self.is_inside(verifiable_range.__end):
-            return Range(verifiable_range.__end, self.__end)
-
-        return [self, verifiable_range]
+        return [Range(self.__start, self.__end), Range(range_2.__start, range_2.__end)]
 
     def __repr__(self):
-        return '[%s;%s]' % (self.__start, self.__end)
-
-
-if __name__ == '__main__':
-    start_point = float(input('Введите начало интервала: '))
-    end_point = float(input('Введите конец интервала: '))
-    point_a = float(input('Введите координату точки А: '))
-
-    current_range = Range(start_point, end_point)
-
-    if current_range.is_inside(point_a):
-        print(f'Точка A находится внутри интервала {current_range}')
-        range_1 = Range(start_point, point_a)
-        range_2 = Range(point_a, end_point)
-        print(
-            f'Расстояние от начала интервала до точки A = {range_1.get_length():.2f}, '
-            f'расстояние от точки A до конца интервала = {range_2.get_length():.2f}')
-    else:
-        print(f'Точка А не находится внутри интервала {current_range}')
+        return '(%s; %s)' % (self.__start, self.__end)
