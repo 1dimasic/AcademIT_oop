9dfbb3c6cd0e4d1c5dad715ed9ea1b5549adbb40
Dmitrii <litrox@mail.ru>
2023-07-27 23:03:07 +0700

    Решение задачи Shape (п.3)

--- shape_task/shape.py
+++ shape_task/shape.py
@@ -22,6 +22,9 @@ class Shape(ABC):
 
 class Square(Shape):
     def __init__(self, side_length):
+        if not isinstance(side_length, int | float):
+            raise TypeError(f'Длина стороны квадрата должна быть числом, а не {type(side_length).__name__}')
+
         self.__side_length = side_length
 
     @property
@@ -38,12 +41,27 @@ class Square(Shape):
     def get_area(self):
         return self.__side_length * self.__side_length
 
+    def __hash__(self):
+        return hash((self.__side_length,))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__side_length == other.__side_length
+
+        return False
+
     def __str__(self):
         return '%s, длина стороны = %s' % (self.__class__.__name__, self.__side_length)
 
 
 class Triangle(Shape):
     def __init__(self, x1, y1, x2, y2, x3, y3):
+        coordinates = (x1, y1, x2, y2, x3, y3)
+
+        for coordinate in coordinates:
+            if not isinstance(coordinate, int | float):
+                raise TypeError(f'Координаты должны быть числом, а не {type(coordinate).__name__}')
+
         self.__x1 = x1
         self.__y1 = y1
         self.__x2 = x2
@@ -75,13 +93,24 @@ class Triangle(Shape):
         return perimetr
 
     def get_area(self):
-        area = 1
         perimetr_half = self.get_perimetr() / 2
+        area = perimetr_half
 
         for side in self.get_sides_length():
             area *= perimetr_half - side
 
-        return math.sqrt(perimetr_half * area)
+        return math.sqrt(area)
+
+    def __hash__(self):
+        return hash((self.__x1, self.__y1, self.__x2, self.__y2, self.__x3, self.__y3))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__x1 == other.__x1 and self.__y1 == other.__y1 and \
+                   self.__x2 == other.__x2 and self.__y2 == other.__y2 and \
+                   self.__x3 == other.__x3 and self.__y3 == other.__y3
+
+        return False
 
     def __str__(self):
         return '%s, координаты вершин (x1,y1) = (%s,%s), (x2,y2) = (%s,%s), (x3,y3) = (%s,%s)' \
@@ -90,6 +119,12 @@ class Triangle(Shape):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
+        if not isinstance(width, int | float):
+            raise TypeError(f'Ширина прямоугольника должна быть числом, а не {type(width).__name__}')
+
+        if not isinstance(height, int | float):
+            raise TypeError(f'Высота прямоугольника должна быть числом, а не {type(height).__name__}')
+
         self.__width = width
         self.__height = height
 
@@ -107,12 +142,24 @@ class Rectangle(Shape):
     def get_area(self):
         return self.__height * self.__width
 
+    def __hash__(self):
+        return hash((self.__width, self.__height))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__width == other.__width and self.__height == other.__height
+
+        return False
+
     def __str__(self):
         return '%s, ширина = %s, высота = %s' % (self.__class__.__name__, self.__width, self.__height)
 
 
 class Circle(Shape):
     def __init__(self, radius):
+        if not isinstance(radius, int | float):
+            raise TypeError(f'Радиус окружности должен быть числом, а не {type(radius).__name__}')
+
         self.__radius = radius
 
     @property
@@ -128,6 +175,15 @@ class Circle(Shape):
 
     def get_area(self):
         return math.pi * pow(self.__radius, 2)
+
+    def __hash__(self):
+        return hash((self.__radius,))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__radius == other.__radius
+
+        return False
 
     def __str__(self):
         return '%s, радиус = %s' % (self.__class__.__name__, self.__radius)
