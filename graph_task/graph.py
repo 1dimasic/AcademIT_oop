c4223fba73ca723ffdfa16218f50fe1d0ff6dad9
Dmitrii <litrox@mail.ru>
2023-09-13 00:16:12 +0700

    graph well done

--- graph_task/graph.py
+++ graph_task/graph.py
@@ -4,59 +4,107 @@ from collections import deque
 class Graph:
     def __init__(self, graph):
         if not isinstance(graph, list):
-            raise TypeError
+            raise TypeError(f'Incorrect type of argument "{type(graph).__name__}", not list')
 
-        self.graph = graph
+        if not all(isinstance(item, int) for row in graph for item in row):
+            raise TypeError(f'Incorrect type for graph item, not int')
+
+        self.__graph = graph
         self.__count = len(graph)
 
-    def visit(self):
+    def visit_in_width(self, function=None):
         visited = [False] * self.__count
+
         graph_deque = deque()
         graph_deque.appendleft(0)
 
         while graph_deque:
-            if visited[graph_deque[-1]]:
-                graph_deque.pop()
+            key = False
+
+            if visited[vertex := graph_deque.pop()]:
                 continue
 
-            print(self.graph[graph_deque[-1]][graph_deque[-1]], end=' ')
-            visited[graph_deque[-1]] = True
+            try:
+                function(self.__graph[vertex][vertex])
+            except TypeError:
+                pass
+
+            visited[vertex] = True
 
             for i in range(self.__count):
-                if graph_deque[-1] == i:
+                if vertex == i:
                     continue
 
-                if self.graph[graph_deque[-1]][i] == 1:
+                if self.__graph[vertex][i] == 1 and not visited[i]:
                     graph_deque.appendleft(i)
+                    key = True
+                    continue
 
-    def visit_1(self):
+            if not key:
+                try:
+                    new_vertex = visited.index(False)
+                    graph_deque.appendleft(new_vertex)
+                except ValueError:
+                    return
+
+    def visit_in_depth(self, function=None):
         visited = [False] * self.__count
         graph_deque = deque()
         graph_deque.appendleft(0)
 
         while graph_deque:
-            print(self.graph[graph_deque[0]][graph_deque[0]], end=' ')
-            index = graph_deque[0]
-            visited[index] = True
-            graph_deque.popleft()
+            key = False
+
+            if visited[vertex := graph_deque.popleft()]:
+                continue
+
+            try:
+                function(self.__graph[vertex][vertex])
+            except TypeError:
+                pass
+
+            visited[vertex] = True
 
             for i in range(self.__count - 1, -1, -1):
-                if index == i:
+                key = False
+
+                if vertex == i:
                     continue
 
-                if self.graph[index][i] == 1 and not visited[i]:
+                if self.__graph[vertex][i] == 1 and not visited[i]:
                     graph_deque.appendleft(i)
+                    key = True
+                    continue
 
-    def visit_2(self, peak=None, v=[False] * 6):
-        if not peak:
-            peak = 0
+            if not key and not graph_deque:
+                try:
+                    new_vertex = visited.index(False)
+                    graph_deque.appendleft(new_vertex)
+                except ValueError:
+                    return
 
-        if v[peak]:
-            return
+    def visit_in_depth_with_recursion(self, vertex, visited=None, function=None):
+        if visited is None:
+            visited = [False] * self.__count
 
-        print(self.graph[peak][peak])
+        try:
+            function(self.__graph[vertex][vertex])
+        except TypeError:
+            pass
+
+        visited[vertex] = True
 
         for i in range(self.__count):
-            if self.graph[peak][i] == 1:
+            if i == vertex:
+                continue
 
-                self.visit_2(i)
+            if self.__graph[vertex][i] == 1 and not visited[i]:
+                self.visit_in_depth_with_recursion(i, visited, function=function)
+
+        try:
+            new_vertex = visited.index(False)
+            self.visit_in_depth_with_recursion(new_vertex, visited, function=function)
+        except ValueError:
+            return
+
+        return
--- graph_task/main.py
+++ graph_task/main.py
@@ -1,7 +1,13 @@
 from graph_task.graph import Graph
 
-graf_list = [[0, 0, 0, 0, 0, 1, 0], [0, 10, 0, 0, 0, 1, 1], [0, 0, 20, 0, 0, 1, 0], [0, 0, 0, 30, 1, 1, 0],
-             [0, 0, 0, 1, 40, 0, 0], [1, 1, 1, 1, 0, 50, 0], [0, 1, 0, 0, 0, 0, 60]]
+graf_list = [[-5, 1, 0, 0, 0, 0, 0, 0], [1, -1, 1, 1, 0, 0, 0, 0], [0, 1, 2, 0, 0, 1, 0, 0], [0, 1, 0, 6, 1, 0, 0, 0],
+             [0, 0, 0, 1, 8, 0, 0, 0], [0, 0, 1, 0, 0, 11, 0, 0], [0, 0, 0, 0, 0, 0, 20, 1],
+             [0, 0, 0, 0, 0, 0, 1, 30]]
 
 graf = Graph(graf_list)
-graf.visit_2()
+
+graf.visit_in_width(print)
+print()
+graf.visit_in_depth(print)
+print()
+graf.visit_in_depth_with_recursion(0, function=print)
