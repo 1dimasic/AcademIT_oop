77fc4a59ea7b53cae0b40a788e205d58841a97e6
Dmitrii <litrox@mail.ru>
2023-10-06 20:40:22 +0700

    minesweeper v 1.0

--- minesweeper_task/logic/controller.py
+++ minesweeper_task/logic/controller.py
@@ -7,31 +7,40 @@ class Controller:
         self.__view = view
         self.__start_time = None
 
-    def prepare(self, size_x=9, size_y=9, mines_count=5):
-        game_field = self.__model.create_game_field(size_x, size_y, mines_count)
-        self.__view.add_widgets_on_game_field(game_field, size_x, size_y)
+    def start(self, size_x=9, size_y=9, mines_count=10):
+        if mines_count > size_x * size_y:
+            raise ValueError(f'Mines count must be <= {size_x*size_y}, not {mines_count}')
+
+        self.__model.create_game_field(size_x, size_y, mines_count)
+        self.__view.add_fields(size_x, size_y)
         self.__start_time = time.time()
 
     def pushed_left_click(self, x, y):
-        self.__model.pushed_left_click(x, y)
+        result = self.__model.pushed_left_click(x, y)
+
+        if result[0] == -2:
+            self.__view.put_away_flag(x, y, result[1])
+
+        if result[0] == -1:
+            self.__view.show_all_mines_and_game_over(result[1])
+
+        if result[0] == 0:
+            self.__view.open_field(result[1])
+
+        if result[0] == 1:
+            self.__view.open_field(x, y, result[1])
 
     def pushed_right_click(self, x, y):
-        self.__model.set_flag(x, y)
+        result = self.__model.put_flag(x, y)
 
-    def game_over(self, minefields):
-        self.__view.show_all_mines_and_game_over(minefields)
+        if result == 1:
+            self.__view.show_winning_message(time.time() - self.__start_time)
 
-    def remove_flag(self, data, x, y):
-        self.__view.hide_flag(data, x, y)
+        if result == 0:
+            self.__view.put_flag(x, y)
 
-    def set_flag(self, x, y):
-        self.__view.show_flag(x, y)
+    def add_to_high_scores(self, name_and_time):
+        self.__model.add_to_high_scores(name_and_time)
 
-    def win_game(self):
-        self.__view.show_winning_message(time.time() - self.__start_time)
-
-    def open_field(self, x, y, data):
-        self.__view.show_field(x, y, data)
-
-    def add_1(self, player_name, game_time):
-        self.__model.add_game_time(player_name, game_time)
+    def load_high_scores(self):
+        return self.__model.load_high_scores()
