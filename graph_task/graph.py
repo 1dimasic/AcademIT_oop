b9142b0c717c9d86291613c5e35c9d3b70cc0dd3
Dmitrii <litrox@mail.ru>
2023-10-10 00:26:39 +0700

    graph

--- graph_task/graph.py
+++ graph_task/graph.py
@@ -2,32 +2,32 @@ from collections import deque
 
 
 class Graph:
-    def __init__(self, graph_matrix):
-        if not isinstance(graph_matrix, list):
-            raise TypeError(f'Incorrect type of argument "{type(graph_matrix).__name__}", not list')
+    def __init__(self, matrix):
+        if not isinstance(matrix, list):
+            raise TypeError(f'Incorrect type of argument "{type(matrix).__name__}", not list')
 
-        if not all(isinstance(row, list) for row in graph_matrix):
-            raise TypeError(f'Incorrect type, must be list')
+        if not all(isinstance(row, list) for row in matrix):
+            raise TypeError(f'Incorrect type of matrix row, must be list')
 
-        max_size = len(max((row for row in graph_matrix), key=len))
-        min_size = len(min((row for row in graph_matrix), key=len))
+        row_max_size = len(max(matrix, key=len))
+        row_min_size = len(min(matrix, key=len))
 
-        if max_size != min_size or max_size != len(graph_matrix):
+        if row_max_size != row_min_size or row_max_size != len(matrix):
             raise ValueError(f'Graph matrix must be square size')
 
-        if not all(isinstance(item, int) for row in graph_matrix for item in row):
+        if not all(isinstance(item, int) for row in matrix for item in row):
             raise TypeError(f'Incorrect type for matrix item, not int')
 
-        self.__graph_matrix = graph_matrix
-        self.__count = len(graph_matrix)
+        self.__matrix = matrix
+        self.__count = len(matrix)
 
     def visit_in_width(self, function):
         visited = [False] * self.__count
         graph_deque = deque()
 
-        while any(not visit for visit in visited):
-            vertex = visited.index(False)
-            graph_deque.appendleft(vertex)
+        for i in range(self.__count):
+            if not visited[i]:
+                graph_deque.appendleft(i)
 
             while len(graph_deque) != 0:
                 vertex = graph_deque.pop()
@@ -35,23 +35,23 @@ class Graph:
                 if visited[vertex]:
                     continue
 
-                function(vertex, end=' ')
+                function(vertex)
                 visited[vertex] = True
 
-                for i in range(self.__count):
-                    if vertex == i:
+                for j in range(self.__count):
+                    if vertex == j:
                         continue
 
-                    if self.__graph_matrix[vertex][i] == 1 and not visited[i]:
-                        graph_deque.appendleft(i)
+                    if self.__matrix[vertex][j] == 1 and not visited[j]:
+                        graph_deque.appendleft(j)
 
     def visit_in_depth(self, function):
         visited = [False] * self.__count
         graph_deque = deque()
 
-        while any(not visit for visit in visited):
-            vertex = visited.index(False)
-            graph_deque.appendleft(vertex)
+        for i in range(self.__count):
+            if not visited[i]:
+                graph_deque.appendleft(i)
 
             while len(graph_deque) != 0:
                 vertex = graph_deque.popleft()
@@ -59,35 +59,30 @@ class Graph:
                 if visited[vertex]:
                     continue
 
-                function(vertex, end=' ')
+                function(vertex)
                 visited[vertex] = True
 
-                for i in range(self.__count - 1, -1, -1):
-                    if vertex == i:
+                for j in range(self.__count - 1, -1, -1):
+                    if vertex == j:
                         continue
 
-                    if self.__graph_matrix[vertex][i] == 1 and not visited[i]:
-                        graph_deque.appendleft(i)
+                    if self.__matrix[vertex][j] == 1 and not visited[j]:
+                        graph_deque.appendleft(j)
 
     def __visit_in_depth_with_recursion(self, vertex, visited, function):
-        function(vertex, end=' ')
+        function(vertex)
         visited[vertex] = True
 
         for i in range(self.__count):
             if i == vertex:
                 continue
 
-            if self.__graph_matrix[vertex][i] == 1 and not visited[i]:
+            if self.__matrix[vertex][i] == 1 and not visited[i]:
                 self.__visit_in_depth_with_recursion(i, visited, function)
-
-        try:
-            new_vertex = visited.index(False)
-            self.__visit_in_depth_with_recursion(new_vertex, visited, function)
-        except ValueError:
-            return
 
     def visit_in_depth_with_recursion(self, function):
         visited = [False] * self.__count
-        vertex = 0
 
-        self.__visit_in_depth_with_recursion(vertex, visited, function)
+        for i in range(self.__count):
+            if not visited[i]:
+                self.__visit_in_depth_with_recursion(i, visited, function)
