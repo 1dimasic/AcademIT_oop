c33b9bb4383bfec004806aadcdd706388597d30e
Dmitrii <litrox@mail.ru>
2023-10-10 00:26:55 +0700

    graph

--- graph_task/main.py
+++ graph_task/main.py
@@ -11,13 +11,13 @@ graf_matrix = [[0, 1, 0, 0, 0, 0, 0, 0],
 
 graf = Graph(graf_matrix)
 
-print('Visit in width:', end=' ')
+print('Visit in width:')
 graf.visit_in_width(print)
 print()
 
-print('Visit in depth:', end=' ')
+print('Visit in depth:')
 graf.visit_in_depth(print)
 print()
 
-print('Visit in depth with recursion:', end=' ')
+print('Visit in depth with recursion:')
 graf.visit_in_depth_with_recursion(print)
