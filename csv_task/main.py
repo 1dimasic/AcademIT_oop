680b2f9d74da8287d9ed1dc16ac93a4241da0161
Dmitrii <litrox@mail.ru>
2023-10-09 23:07:07 +0700

    csv

--- csv_task/main.py
+++ csv_task/main.py
@@ -1,4 +1,6 @@
-from csv_task.csv import CSV
+from csv_task.converter_csv_to_html import ConverterCsvToHtml
 
-csv_to_html = CSV()
-csv_to_html.read_csv()
+input_file = 'D:\\Python\\AcademIT_oop\\csv_task\\input.csv'
+output_file_name = 'output.html'
+csv_to_html = ConverterCsvToHtml()
+csv_to_html.convert(input_file, output_file_name)
